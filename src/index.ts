dc660cff80072ab3165dde73ad2da21645538764
Mathias Dietrich <mathias.dietrich@tech41.de>
2025-01-21 10:16:55 +0100

    init

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,27 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+import { Context, Hono } from 'hono';
+import { cors } from 'hono/cors';
+
+export type Env = {
+	INQUEUE: Queue<any>;
+};
+
+const app = new Hono();
+app.use(
+	'*',
+	cors({
+		origin: ['https://plinken.com', 'https://home.plinken.com', 'http://localhost:3000', 'http://localhost:5173'],
+		allowHeaders: ['Authorization', 'X-Custom-Header', 'Content-Type', 'Upgrade-Insecure-Requests'],
+		allowMethods: ['POST', 'PUT', 'GET', 'OPTIONS', 'DELETE'],
+		exposeHeaders: ['Content-Length', 'X-Kuma-Revision'],
+		maxAge: 600,
+		credentials: true,
+	})
+);
+
+app.post('/0/inqueue', async (c: Context) => {
+	const body = await c.req.json();
+	await c.env.INQUEUE.send(body);
+});
